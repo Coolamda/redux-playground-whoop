3650ae51d411ac47451b7eb842329d0526364ab3
Liam <lw@esyoil.com>
2018-06-18 19:43:59 +0200

    Add some styling to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 import Posts from "./containers/Posts";
 
+const App = styled.div`
+  width: 60%;
+  margin: 1rem auto 0;
+`;
+
+const Heading = styled.h1`
+  border-bottom: 1px solid #eee;
+  padding-bottom: 0.5rem;
+`;
+
 export default () => (
-  <div className="App">
-    <h1>Posts</h1>
+  <App>
+    <Heading>Posts</Heading>
     <Posts />
-  </div>
+  </App>
 );
