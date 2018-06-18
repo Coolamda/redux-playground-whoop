9112143fc65c54426bf6a50052fbaac15e5fa8f7
Liam <lw@esyoil.com>
2018-06-18 19:34:44 +0200

    Add typographyjs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import Typography from "typography";
+import funstonTheme from "typography-theme-funston";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import postsReducer from "./reducers/postsReducer";
+
+funstonTheme.baseFontSize = "18px";
+const typography = new Typography(funstonTheme);
+
+typography.injectStyles();
 
 const rootReducer = combineReducers({
   posts: postsReducer
